93a2c80334e63a995985fd7ca776d9f30ff3cadc
Roman Kornilov <kornilovroman123@gmail.com>
2023-09-22 22:18:48 +0500

    [#12] Fix according to code review

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,23 @@ int main(int argc, char** argv)
 {
     logger::init(logger::debug, logger::cout);
 
-    if(argc != 4)
-    {
-        std::cerr <<
+    if (argc == 1) {
+        std::cout <<
                   "Usage: coolstory_gram_client <host> <port> <text>\n" <<
                   "Example:\n" <<
                   "    coolstory_gram_client echo.websocket.org 80 \"Hello, world!\"\n";
         return EXIT_SUCCESS;
     }
+    if (argc != 4) {
+        std::cerr <<
+                  "Usage: coolstory_gram_client <host> <port> <text>\n" <<
+                  "Example:\n" <<
+                  "    coolstory_gram_client echo.websocket.org 80 \"Hello, world!\"\n";
+        return EXIT_FAILURE;
+    }
     auto const host = argv[1];
     auto const port = argv[2];
     auto const text = argv[3];
-
     run(host, port, text);
-
     return EXIT_SUCCESS;
 }
